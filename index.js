2b4a89aaf701e356c81c934321f2d5103c366d82
alekseynd <adementyevn@gmail.com>
2017-09-01 00:43:48 +0300

    post request added

--- index.js
+++ index.js
@@ -1,290 +1,3 @@
-// document.addEventListener("DOMContentLoaded", ready);
-//
-//
-// var resultContainer;
-// function ready() {
-//
-// }
-//
-// function submit(e) {
-//
-//     // myForm.validate();
-//     // e.preventDefault();
-//
-//     // validateFio(dataFio.value);
-//     // validateEmail(dataEmail.value);
-//     // validatePhone(dataPhone.value);
-//
-//     // makeRequest ()
-//     // function makeRequest() {
-//     //     if (true) {
-//     //         console.log('dataFioValidated && dataEmailValidated && dataEmailValidated')
-//     //
-//     //         var filesArray = [
-//     //             'success',
-//     //             'error',
-//     //             'progress'
-//     //         ];
-//     //         function getRandom(min, max) {
-//     //             return Math.floor(Math.random() * (max - min) + min);
-//     //         }
-//     //         var addresName = filesArray[getRandom(0, 3)]
-//     //
-//     //         var xmlHttp= new XMLHttpRequest();
-//     //
-//     //         xmlHttp.open('GET', addresName+'.json', true);
-//     //
-//     //         xmlHttp.send();
-//     //
-//     //         xmlHttp.onreadystatechange = function() {
-//     //             if (xmlHttp.readyState != 4) return;
-//     //
-//     //              submitButton.disabled = true;
-//     //
-//     //             if (xmlHttp.status != 200) {
-//     //                 // обработать ошибку
-//     //                 alert(xmlHttp.status + ': ' + xmlHttp.statusText);
-//     //             }
-//     //             else {
-//     //                 submitButton.disabled = false;
-//     //                 var response = JSON.parse(xmlHttp.response);
-//     //                 console.log(JSON.parse(xmlHttp.response));
-//     //                 if (response.status === 'success') {
-//     //                     resultContainer.classList.remove('error');
-//     //                     resultContainer.classList.add('success');
-//     //                     resultContainer.innerHTML = 'success'
-//     //                 }
-//     //                 else if (response.status === 'error') {
-//     //                     resultContainer.classList.remove('success');
-//     //                     resultContainer.classList.add('error');
-//     //                     resultContainer.innerHTML = response.reason
-//     //                 }
-//     //                 else {
-//     //                     resultContainer.removeAttribute('class')
-//     //                     submitButton.disabled = true;
-//     //                     setTimeout(makeRequest, response.timeout)
-//     //
-//     //                 }
-//     //
-//     //             }
-//     //         }
-//     //     }
-//     // }
-//
-//
-//
-// }
-//
-// // console.log(dataFio.classList.add('error'))
-// var validFioValid  = true;
-// var dataEmailValid = true;
-// var dataPhoneValid = true;
-//
-//
-//
-//
-// // function validateFio(argFio) {
-// //     if ( /^[A-z\u0400-\u04FF]{1,30}\s+[A-z\u0400-\u04FF]{1,30}\s+[A-z\u0400-\u04FF]{1,30}\s*$/.test(argFio)) {
-// //         dataFioValidated = true;
-// //     }
-// //     else {
-// //         dataFioValidated = false;
-// //
-// //         dataFio.classList.add('error')
-// //     }
-// // }
-//
-//
-// // function validateEmail(email) {
-// //     var validDomens = [
-// //         '@ya.ru',
-// //         '@yandex.ru',
-// //         '@yandex.ua',
-// //         '@yandex.by',
-// //         '@yandex.kz',
-// //         '@yandex.com'
-// //     ];
-// //     for (var i = 0; i < validDomens.length; i++) {
-// //         var pattern ='[A-z]{3,30}';
-// //         var reg = new RegExp(pattern+validDomens[i]);
-// //
-// //         if(reg.test(email)) {
-// //             //    stop ex and errer
-// //             dataEmailValidated = true;
-// //             console.log('A')
-// //             break;
-// //         }
-// //         else {
-// //             dataEmailValidated = false
-// //             dataEmail.classList.add('error')
-// //         }
-// //     }
-// // }
-// //
-// // function validatePhone(em) {
-// //     var regExpMail = /^\+7\(\d{3}\)\d{3}\-\d{2}\-\d{2}$/;
-// //     if (regExpMail.test(em)) {
-// //         var sum = 0;
-// //         var nums = em.match(/\d+/g).join('')
-// //         for (var i = 0; i < nums.length; i++) {
-// //             sum += +nums[i];
-// //
-// //         }
-// //         if (sum > 30) {
-// //             dataPhoneValidated = false;
-// //             dataPhone.classList.add('error')
-// //         }
-// //         else {
-// //             dataPhoneValidated = true;
-// //             dataPhone.classList.remove('error')
-// //         }
-// //     }
-// //     else {
-// //         dataPhoneValidated = false;
-// //         dataPhone.classList.add('error')
-// //     }
-// // }
-//
-//
-// var myForm = {
-//    // form:  document.getElementById('myForm'),
-//    //  formaa: document.getElementById('myForm'),
-// // var form ;
-// //
-// // form = document.getElementById('myForm');
-// // resultContainer = document.getElementById('resultContainer');
-// // //submitButton
-// // var submitButton = document.getElementById('submitButton');
-// // submitButton.addEventListener('click', submit);
-// //
-// // var dataFio ;
-// // var dataEmail;
-// // var dataPhone;
-// // dataFio   = form['fio'];
-// // dataEmail = form['email'];
-// // dataPhone = form['phone'];
-// // dataFioValue   = dataFio.value;
-// // dataEmailValue = dataEmail.value;
-// // dataPhoneValue = dataPhone.value;
-//
-//
-//     validate: function () {
-//         var validatedForm = {
-//             isValid: Boolean,
-//             errorFields: []
-//         };
-//
-//         console.log(this.formaa);
-//
-//         function validateFio(argFio) {
-//             console.log(form['fio'].value)
-//             if ( /^[A-z\u0400-\u04FF]{1,30}\s+[A-z\u0400-\u04FF]{1,30}\s+[A-z\u0400-\u04FF]{1,30}\s*$/.test(argFio)) {
-//                 // dataFioValidated = true;
-//
-//                 if(validForm.indexOf('fio') !== -1) {
-//                     validForm.errorFields.splice(validForm.indexOf('fio'), 1 )
-//                 }
-//             }
-//             else {
-//                 // dataFioValidated = false;
-//                 validForm.errorFields.push('fio');
-//                 dataFio.classList.add('error')
-//             }
-//         };
-//
-//         function validateEmail(email) {
-//             var validDomens = [
-//                 '@ya.ru',
-//                 '@yandex.ru',
-//                 '@yandex.ua',
-//                 '@yandex.by',
-//                 '@yandex.kz',
-//                 '@yandex.com'
-//             ];
-//             for (var i = 0; i < validDomens.length; i++) {
-//                 var pattern ='[A-z]{3,30}';
-//                 var reg = new RegExp(pattern+validDomens[i]);
-//
-//                 if(reg.test(email)) {
-//                     //    stop ex and errer
-//                     // dataEmailValidated = true;
-//
-//                     if(validForm.indexOf('email') !== -1) {
-//                         validForm.errorFields.splice(validForm.indexOf('email'), 1 )
-//                     }
-//
-//                     dataEmail.classList.remove('error')
-//                     break;
-//                 }
-//                 else {
-//                     validForm.errorFields.push('email');
-//                     dataEmail.classList.add('error')
-//                 }
-//             }
-//         };
-//
-//         function validatePhone(em) {
-//             var regExpMail = /^\+7\(\d{3}\)\d{3}\-\d{2}\-\d{2}$/;
-//             if (regExpMail.test(em)) {
-//                 var sum = 0;
-//                 var nums = em.match(/\d+/g).join('')
-//                 for (var i = 0; i < nums.length; i++) {
-//                     sum += +nums[i];
-//
-//                 }
-//                 if (sum > 30) {
-//                     dataPhoneValidated = false;
-//                     validForm.errorFields.push('phone');
-//                     dataPhone.classList.add('error')
-//                 }
-//                 else {
-//                     dataPhoneValidated = true;
-//
-//                     if(validForm.indexOf('phone') !== -1) {
-//                         validForm.errorFields.splice(validForm.indexOf('phone'), 1 )
-//                     }
-//
-//                     dataPhone.classList.remove('error')
-//                 }
-//             }
-//             else {
-//                 dataPhoneValidated = false;
-//                 validForm.errorFields.push('phone');
-//                 dataPhone.classList.add('error')
-//             }
-//         };
-//         // dataFioValue   = dataFio.value;
-//         // dataEmailValue = dataEmail.value;
-//         // dataPhoneValue)
-//         validatePhone(dataPhoneValue);
-//         // coonsole.log(dataFio);
-//         validateFio(dataFioValue);
-//         validateEmail(dataEmailValue);
-//         if (validForm.errorFields.length !== 0) {
-//             validForm.isValid = false;
-//         }
-//         else {
-//             validForm.isValid = true;
-//         }
-//         console.log(validForm);
-//
-//     },
-//     getData: function () {
-//         return obj;
-//     },
-//     setData: function (obj) {
-//
-//     },
-//     submit: function () {
-//         // this.myForm.validate();
-//         // e.preventDefault();
-//     }
-// };
-
-
-
-
-
 document.addEventListener("DOMContentLoaded", function () {
     formObject = document.getElementById('myForm');
     submitButton = document.getElementById('submitButton');
@@ -325,10 +38,8 @@ var myForm = {
 
         function validateEmail() {
             if (emailValue.trim().length) {
-                testedEmailValue = emailValue.split(' ').join('');
-                var testedEmailValue;
-                console.log(emailValue);
-                if (/^([\w\d]{3,30}[@]{1}[ya]{2}[.]{1}[ru]{2})$|^([\w\d]{3,30}[@]{1}[yandex]{6}[.]{1}[ru|ua|by|kz|com]{2})$/.test(testedEmailValue)) {
+                if ( (/@ya.ru{1}/.test(emailValue) || /@yandex.(ru|ua|by|kz|com){1}/.test(emailValue)) && /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(emailValue)) {
+
                     if(validatedForm.errorFields.indexOf('email') !== -1) {
                         validatedForm.errorFields.splice(validatedForm.errorFields.indexOf('email'), 1 );
                     }
@@ -373,13 +84,11 @@ var myForm = {
         validateEmail();
         validatePhone();
         validatedForm.isValid = validatedForm.errorFields.length > 0 ? false : true;
-        console.log(validatedForm);
         return validatedForm;
     },
     submit: function(e) {
         e.preventDefault();
         if (myForm.validate().isValid) {
-            console.log(JSON.stringify(myForm.getData()));
             makeRequest();
         }
         function makeRequest() {
@@ -392,25 +101,26 @@ var myForm = {
                 return Math.floor(Math.random() * (max - min) + min);
             }
             var addresName = filesArray[getRandom(0, 3)];
-
             var xmlHttp= new XMLHttpRequest();
 
+            // POST Request with form params
+
+            // var params = JSON.stringify(myForm.getData());
+            // xmlHttp.open("POST", addresName+'.json', true);
+            // xmlHttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            // xmlHttp.send(params)
+
             xmlHttp.open('GET', addresName+'.json', true);
-
             xmlHttp.send();
-
             xmlHttp.onreadystatechange = function() {
                 if (xmlHttp.readyState != 4) return;
-
                 submitButton.disabled = true;
-
                 if (xmlHttp.status != 200) {
                     alert(xmlHttp.status + ': ' + xmlHttp.statusText);
                 }
                 else {
                     submitButton.disabled = false;
                     var response = JSON.parse(xmlHttp.response);
-                    console.log(JSON.parse(xmlHttp.response));
                     if (response.status === 'success') {
                         resultContainer.className = 'success';
                         resultContainer.innerHTML = response.status
--- server.js
+++ server.js
@@ -32,7 +32,7 @@ http.createServer(function (request, response) {
     fs.readFile(filePath, function(error, content) {
         if (error) {
             if(error.code == 'ENOENT'){
-                fs.readFile('../404.html', function(error, content) {
+                fs.readFile('404.html', function(error, content) {
                     response.writeHead(200, { 'Content-Type': contentType });
                     response.end(content, 'utf-8');
                 });
